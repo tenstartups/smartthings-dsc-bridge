0df066fe82e2c3d66e04476d30b99b7a7f7415e7
Marc Lennox <marc.lennox@gmail.com>
2019-03-18 19:01:44 -0400

    Added multi-arch builds

---
+++ downlaod-qemu.sh
@@ -0,0 +1,3 @@
+wget -q -O qemu-arm-static.tar.gz https://github.com/multiarch/qemu-user-static/releases/download/v2.12.0-1/qemu-arm-static.tar.gz
+tar xvzf qemu-arm-static.tar.gz
+rm qemu-arm-static.tar.gz
